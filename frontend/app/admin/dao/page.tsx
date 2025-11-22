98865de4ef1246950e922ff9ad8300908e7521e2
nodexxplorer <royaldivinewill24@gmail.com>
2025-11-22 19:55:23 +0100

    ten

--- frontend/app/admin/dao/page.tsx
+++
@@ -1,9 +0,0 @@
-import DAODashboard from "@/components/DAODashboard";
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-gray-900 text-white">
-      <DAODashboard />
-    </main>
-  );
-}
